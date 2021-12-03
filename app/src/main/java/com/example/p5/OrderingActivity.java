25cc33b3fd4226103fbaffed0c4037f58bcc1424
yeetstatic <balitinna@gmail.com>
2021-12-02 20:09:43 -0500

    57

---
+++ app/src/main/java/com/example/p5/OrderingActivity.java
@@ -0,0 +1,50 @@
+package com.example.p5;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.p5.ui.main.SectionsPagerAdapter2;
+import com.example.p5.databinding.ActivityOrderingBinding;
+
+public class OrderingActivity extends AppCompatActivity {
+
+    private ActivityOrderingBinding binding;
+    TextView editTextPhone2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityOrderingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Intent i=getIntent();
+        editTextPhone2=(TextView)  findViewById(R.id.editTextPhone2);
+        String number= i.getExtras().getString("number");
+        editTextPhone2.setText(number);
+        SectionsPagerAdapter2 sectionsPagerAdapter = new SectionsPagerAdapter2(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/p5/ui/main/SectionsPagerAdapter2.java
@@ -0,0 +1,61 @@
+package com.example.p5.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.p5.R;
+import com.example.p5.fragment_current;
+import com.example.p5.fragment_home;
+import com.example.p5.fragment_login;
+import com.example.p5.fragment_menu;
+import com.example.p5.fragment_orders;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter2 extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_3,R.string.tab_text_4};
+    private final Context mContext;
+
+    public SectionsPagerAdapter2(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment=null;
+        int first=0;
+        int second=1;
+        if(position==first){
+            fragment= new fragment_menu();
+        }
+        else if(position==second){
+            fragment= new fragment_current();
+        }
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        int num=2;
+        return num;
+    }
+}
