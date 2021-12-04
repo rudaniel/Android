1df013d5867cc20090e663d03dfcebcb47504e41
rudaniel <daniel.lop1002@gmail.com>
2021-12-03 22:36:06 -0500

    rape

--- app/src/main/java/com/example/p5/fragment_current.java
+++ app/src/main/java/com/example/p5/fragment_current.java
@@ -1,5 +1,6 @@
 package com.example.p5;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,9 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,15 +67,45 @@ public class fragment_current extends Fragment {
         }
     }
 
+
+
+
+    ListView pizzaList;
+
+    ArrayList<String> pizzas =  new ArrayList<String>();
+    ArrayAdapter<String> adapterPizzas;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_current, container, false);
-        phoneTemp = (TextView) view.findViewById(R.id.phoneTemp);
         phoneHolder = (TextView) getActivity().findViewById(R.id.editTextPhone2);
         phoneNumber= phoneHolder.getText().toString();
-        phoneTemp.setText(phoneNumber);
+
+        pizzaList = (ListView) view.findViewById(R.id.pizzaList);
+
+        pizzas.add(phoneNumber); //Instead of adding phoneNumber we will add all of the pizzas to the list.
+        pizzas.add("9086342536");
+        pizzas.add("1010203040");
+
+
+
+        adapterPizzas = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1 , pizzas);
+        pizzaList.setAdapter(adapterPizzas);
+
+        pizzaList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              //  view.getFocusables(position);
+              //  view.setSelected(true);
+
+
+
+            }
+        });
+        
         getPizzas();
         return view;
     }
