22a29e143771a6effd2fe2472a40abce5a3c3c0c
rudaniel <daniel.lop1002@gmail.com>
2021-12-06 16:40:49 -0500

    papi

--- app/src/main/java/com/example/p5/fragment_menu.java
+++ app/src/main/java/com/example/p5/fragment_menu.java
@@ -17,60 +17,50 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import project4.Order;
-//Manav's Comment
+
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_login#newInstance} factory method to
- * create an instance of this fragment.
+ * Current orders based on current phone number.
+ * @author Manav Bali
+ * @author Daniel Lopez
  */
+
 public class fragment_menu extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
     private static final int request=0;
     private static final int result=-1;
     Order order=null;
-
-    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
     Button deluxe;
     Button hawaiian;
     Button pepperoni;
-
     TextView phoneHolder;
     String phoneNumber=null;
+
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_home.
+     * Sets the fragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static fragment_menu newInstance(String param1, String param2) {
         fragment_menu fragment = new fragment_menu();
         Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
+    /**
+     * Default constructor.
+     */
     public fragment_menu() {}
 
+    /**
+     * Gets the saved instance.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
+    /**
+     * Based on the button asked a page will open with a certain pizza picture.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_menu, container, false);
@@ -87,7 +77,6 @@ public class fragment_menu extends Fragment {
                 startActivityForResult(i,request);
             }
         });
-
         hawaiian= (Button) view.findViewById(R.id.button2);
         hawaiian.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
@@ -97,7 +86,6 @@ public class fragment_menu extends Fragment {
                 startActivityForResult(i,request);
             }
         });
-
         pepperoni= (Button) view.findViewById(R.id.button3);
         pepperoni.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
@@ -110,6 +98,9 @@ public class fragment_menu extends Fragment {
         return view;
     }
 
+    /**
+     * Getting any possible orders.
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
