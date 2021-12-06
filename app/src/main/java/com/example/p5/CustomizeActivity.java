63cf197fbed9ef6414506d7b0e90be2d4e3faf9b
rudaniel <daniel.lop1002@gmail.com>
2021-12-06 16:07:07 -0500

    papi

--- app/src/main/java/com/example/p5/CustomizeActivity.java
+++ app/src/main/java/com/example/p5/CustomizeActivity.java
@@ -28,6 +28,11 @@ import project4.Pizza;
 import project4.Size;
 import project4.Topping;
 
+/**
+ * Controls the menu ordering page.
+ * @author Manav Bali
+ * @author Daniel Lopez
+ */
 public class CustomizeActivity extends AppCompatActivity {
     ArrayAdapter<Size> sizes;
     ArrayList<Size> s= new ArrayList<>(Arrays.asList(Size.values()));
@@ -54,11 +59,47 @@ public class CustomizeActivity extends AppCompatActivity {
     Order order=null;
     TextView livePrice;
 
+    /**
+     * Controls all of the functions and buttons on the page.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(android.R.style.Theme);
         setContentView(R.layout.activity_customize);
+
+        make();
+
+        addToOrderButton.setOnClickListener(new View.OnClickListener() { //When the button is clicked the method will run.
+            public void onClick(View v){
+              order();
+            }
+        });
+        toppingsEditor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                topping(position);
+            }
+        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Pizza temp = makePizza(tempList);
+                livePrice.setText(String.valueOf(temp.price()));
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Pizza temp = makePizza(tempList);
+                livePrice.setText(String.valueOf(temp.price()));
+            }
+        });
+    }
+
+    /**
+     * Connects buttons to xml and gets intent.
+     */
+    public void make() {
         i=getIntent();
         pizza= i.getExtras().getString("pizza");
         order=(Order) i.getSerializableExtra("Order");
@@ -69,65 +110,45 @@ public class CustomizeActivity extends AppCompatActivity {
         livePrice = (TextView) findViewById(R.id.livePrice);
         imageSet();
         spinner.setAdapter(sizes);
-
-        //daniel
-      //  phoneHolder = (TextView) findViewById(R.id.editTextPhone2);
-      //  phoneNumber = phoneHolder.getText().toString();
         toppingsEditor = (ListView) findViewById(R.id.toppingsEditor);
-        Collections.addAll(toppings, project4.Topping.Pineapple, project4.Topping.Ham, project4.Topping.Cheese,
-                project4.Topping.Sausage, project4.Topping.Chicken, project4.Topping.Beef, project4.Topping.Pepperoni,
-                project4.Topping.GreenPepper, project4.Topping.Onion, project4.Topping.Mushroom, project4.Topping.BlackOlives);
+        Collections.addAll(toppings, project4.Topping.Pineapple, project4.Topping.Ham, project4.Topping.Cheese, project4.Topping.Sausage, project4.Topping.Chicken, project4.Topping.Beef, project4.Topping.Pepperoni, project4.Topping.GreenPepper, project4.Topping.Onion, project4.Topping.Mushroom, project4.Topping.BlackOlives);
         adapterTopping = new ArrayAdapter<>(this,  android.R.layout.simple_list_item_multiple_choice , toppings);
         toppingsEditor.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         toppingsEditor.setAdapter(adapterTopping);
-
         addToOrderButton = (Button) findViewById(R.id.addToOrderButton);
-        addToOrderButton.setOnClickListener(new View.OnClickListener() { //When the button is clicked the method will run.
-            public void onClick(View v){
-                tempList.removeAll(Collections.singleton(null));
-                finalPizza = makePizza(tempList);
-                order.addPizza(finalPizza);
-                Intent send= new Intent();
-                send.putExtra("Order", order);
-                setResult(RESULT_OK, send);
-                finish();
-            }
-        });
-
-        toppingsEditor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                temp = (Topping) (toppingsEditor.getItemAtPosition(position));
-                if(tempList.contains(temp)){
-                    tempList.remove(temp);
-                }
-                else{
-                    tempList.add(temp);
-                }
-                Pizza temp = makePizza(tempList);
-                livePrice.setText(String.valueOf(temp.price()));
-
-            }
-        });
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Pizza temp = makePizza(tempList);
-                livePrice.setText(String.valueOf(temp.price()));
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Pizza temp = makePizza(tempList);
-                livePrice.setText(String.valueOf(temp.price()));
-            }
-        });
-
-
-
     }
 
+    /**
+     * Stores the toppings.
+     */
+    public void topping(int position) {
+        temp = (Topping) (toppingsEditor.getItemAtPosition(position));
+        if(tempList.contains(temp)){
+            tempList.remove(temp);
+        }
+        else{
+            tempList.add(temp);
+        }
+        Pizza temp = makePizza(tempList);
+        livePrice.setText(String.valueOf(temp.price()));
+    }
+
+    /**
+     * Passes the pizza to the other fragment.
+     */
+    public void order() {
+        tempList.removeAll(Collections.singleton(null));
+        finalPizza = makePizza(tempList);
+        order.addPizza(finalPizza);
+        Intent send= new Intent();
+        send.putExtra("Order", order);
+        setResult(RESULT_OK, send);
+        finish();
+    }
+
+    /**
+     * Based on type of pizza that is being made the picture will represent that.
+     */
     public void imageSet() {
         String uri="";
         if(pizza.equals(deluxe)){
@@ -147,6 +168,9 @@ public class CustomizeActivity extends AppCompatActivity {
         image.setImageDrawable(res);
     }
 
+    /**
+     * Creates the pizza with inputted toppings.
+     */
     public Pizza makePizza(ArrayList<Topping> toppings) {
         Pizza currentPie = null;
         Size tempSize = (Size) spinner.getSelectedItem();
@@ -165,7 +189,6 @@ public class CustomizeActivity extends AppCompatActivity {
             currentPie.setToppings(toppings);
             currentPie.setSize(tempSize);
         }
-
         return currentPie;
     }
 }
